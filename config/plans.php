567c75db797ce711e08430a7fa4497c19311e376
haythamac <janssen.uy.dev@gmail.com>
2026-03-06 11:54:19 +0800

    feat: added stripe keys and created plans config

---
+++ config/plans.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'free' => [
+        'name' => 'Free',
+        'price' => 0,
+        'features' => [
+            'Feature 1',
+            'Feature 2',
+        ],
+
+    ],
+    'pro' => [
+        'name' => 'Pro',
+        'price' => 19,
+        'stripe_id' => 'price_1T7pK5PFg7zaklYZoYgdObmL',
+        'features' => [
+            'Feature 1',
+            'Feature 2',
+            'Feature 3',
+        ],
+    ],
+    'business' => [
+        'name' => 'Business',
+        'price' => 49,
+        'stripe_id' => 'price_1T7pKJPFg7zaklYZFuJUP5sA',
+        'features' => [
+            'Feature 1',
+            'Feature 2',
+            'Feature 3',
+        ],
+    ]
+];
